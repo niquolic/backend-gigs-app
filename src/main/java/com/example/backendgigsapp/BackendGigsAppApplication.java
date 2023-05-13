6b16ee7d557ef7857efeb75eee705fca74c30ba6
niquolic <nicolas.debruycker22@gmail.com>
2023-05-13 15:48:40 -0400

    init

---
+++ src/main/java/com/example/backendgigsapp/BackendGigsAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendgigsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendGigsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendGigsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backendgigsapp/BackendGigsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendgigsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendGigsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
